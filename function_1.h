f35bbf7ce4c8a8fd554cb59a0fa362e14441a805
afm97 <arthurfranca97@gmail.com>
2023-11-23 18:09:21 -0300

    Inseridas novas funcoes de validacao no return comentado

--- function_1.h
+++ function_1.h
@@ -1,5 +1,5 @@
 double func_1(double time, double y_1, double y_2)
 {
     return (-4 * y_1 + 3 * y_2 + 6);
-    // return (time * pow(e, 3 * time) - 2 * y);
+    // return (-0.4427 * pow(y_1, 0.5) + 0.23 * (4 + 4 * sin(time)));
 }
--- function_2.h
+++ function_2.h
@@ -1,5 +1,5 @@
 double func_2(double time, double y_1, double y_2)
 {
     return (-2.4 * y_1 + 1.6 * y_2 + 3.6);
-    // return (time * pow(e, 3 * time) - 2 * y);
+    // return (-0.4427 * pow(y_2, 0.5) + 0.4427 * pow(y_1, 0.5));
 }
