87c4084463895a4a47b47a4828747a23746dde63
afm97 <arthurfranca97@gmail.com>
2023-11-23 17:58:36 -0300

    Inseridas funcoes para as duas expressoes do sistema linear

---
+++ function_1.h
@@ -0,0 +1,5 @@
+double func_1(double time, double y_1, double y_2)
+{
+    return (-4 * y_1 + 3 * y_2 + 6);
+    // return (time * pow(e, 3 * time) - 2 * y);
+}
---
+++ function_2.h
@@ -0,0 +1,5 @@
+double func_2(double time, double y_1, double y_2)
+{
+    return (-2.4 * y_1 + 1.6 * y_2 + 3.6);
+    // return (time * pow(e, 3 * time) - 2 * y);
+}
